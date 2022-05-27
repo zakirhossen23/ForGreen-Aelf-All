1216d9bef353d2cc99685ae0b998044282c7d4be
zakirhossen23 <73053667+zakirhossen23@users.noreply.github.com>
2022-05-27 10:30:58 +0600

    Initial commit

---
+++ DemeterGift Aelf Contract/test/DemeterGift.Tests/DemeterGiftContractTestModule.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.IO;
+using AElf.Boilerplate.TestBase;
+using AElf.ContractTestBase;
+using AElf.Kernel.SmartContract.Application;
+using Microsoft.Extensions.DependencyInjection;
+using Volo.Abp;
+using Volo.Abp.Modularity;
+
+namespace DemeterGift
+{
+    [DependsOn(typeof(MainChainDAppContractTestModule))]
+    public class DemeterGiftContractTestModule : MainChainDAppContractTestModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            context.Services.AddSingleton<IContractInitializationProvider, DemeterGiftContractInitializationProvider>();
+        }
+
+        public override void OnPreApplicationInitialization(ApplicationInitializationContext context)
+        {
+            var contractCodeProvider = context.ServiceProvider.GetService<IContractCodeProvider>();
+            var contractDllLocation = typeof(DemeterGiftContract).Assembly.Location;
+            var contractCodes = new Dictionary<string, byte[]>(contractCodeProvider.Codes)
+            {
+                {
+                    new DemeterGiftContractInitializationProvider().ContractCodeName,
+                    File.ReadAllBytes(contractDllLocation)
+                }
+            };
+            contractCodeProvider.Codes = contractCodes;
+        }
+    }
+}
