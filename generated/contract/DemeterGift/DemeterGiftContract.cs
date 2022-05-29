26eb39f175d4797805a8e03bd33e5742687f0513
zakirhossen23 <73053667+zakirhossen23@users.noreply.github.com>
2022-05-29 08:37:51 +0600

    Bids

--- generated/contract/DemeterGift/DemeterGiftContract.cs
+++ generated/contract/DemeterGift/DemeterGiftContract.cs
@@ -24,23 +24,22 @@ namespace DemeterGift
             return new StringValue { Value = "Hello World!" };
         }
 
+
+
         /// <summary>
         /// Events
         /// </summary>
-        #region "Events"
         public override StringValue CreateEvent(StringValue input)
         {
             State.EventUris[State.EventIds.Value] = input.Value;
             State.EventIds.Value = State.EventIds.Value + 1;
             return new StringValue { Value = State.EventIds.Value.ToString() };
         }
-
         public override StringValue getTotalEvent(Empty input)
         {
 
             return new StringValue { Value = State.EventIds.Value.ToString() };
         }
-
         public override StringValue getOneEvent(StringValue input)
         {
 
@@ -48,25 +47,22 @@ namespace DemeterGift
 
             return new StringValue { Value = currnetEvent };
         }
-        #endregion
+
 
 
         /// <summary>
         /// Tokens
         /// </summary>
-        #region "Tokens"
         public void CreateToken(string input) //Background
         {
             State.TokenUris[State.TokenIds.Value] = input;
             State.TokenIds.Value = State.TokenIds.Value + 1;
         }
-
         public override StringValue getTotalToken(Empty input)
         {
 
             return new StringValue { Value = State.TokenIds.Value.ToString() };
         }
-
         public override StringValue getOneToken(StringValue input)
         {
 
@@ -74,23 +70,44 @@ namespace DemeterGift
 
             return new StringValue { Value = currnetToken };
         }
-        #endregion
+
+
+
+
+        /// <summary>
+        /// Bids
+        /// </summary>        
+        public void CreateBid(string input) //Background
+        {
+            State.BidUris[State.BidIds.Value] = input;
+            State.BidIds.Value = State.BidIds.Value + 1;
+        }
+        public override StringValue getTotalBid(Empty input)
+        {
+
+            return new StringValue { Value = State.BidIds.Value.ToString() };
+        }
+        public override StringValue getOneBid(StringValue input)
+        {
+
+            var currnetBid = State.BidUris[int.Parse(input.Value)];
+
+            return new StringValue { Value = currnetBid };
+        }
+
 
 
 
         /// <summary>
         /// Event + Tokens
         /// </summary>
-        #region "Event + Tokens"
-        //Background
-        public void setAllEventToken(int EventTokenID, int EventID, string TokenURI)
+        public void setAllEventToken(int EventTokenID, int EventID, string TokenURI)   //Background
         {
             var togehter = new Google.Protobuf.Collections.MapField<int, string>();
             togehter[EventID] = TokenURI;
             State.AllEventTokens[EventTokenID] = new EventToken { EventID = EventID, TokenURI = TokenURI };
-        }
-        //inserting
-        public override Int32Value InsertAllEventToken(InsertEventTokenInput input)
+        }       
+        public override Int32Value InsertAllEventToken(InsertEventTokenInput input)  //inserting
         {
             string tokenURI = input.TokenURI;
             string eventID = input.EventID;
@@ -100,12 +117,9 @@ namespace DemeterGift
             State.EventTokenIds.Value = State.EventTokenIds.Value + 1;
             return new Int32Value { Value = State.TokenIds.Value };
         }
-
-        //Searching
-        public override SearchedList SearchAllTokenByEventID(StringValue eventID)
+        public override SearchedList SearchAllTokenByEventID(StringValue eventID)  //Searching
         {
             var searchedList = new SearchedList();
-
             for (int i = 0; i < State.EventTokenIds.Value; i++)
             {
                 var EventToken = State.AllEventTokens[i];
@@ -114,12 +128,45 @@ namespace DemeterGift
                     searchedList.Tokens.Add(EventToken.TokenURI);
                 }
             }
-      
             return searchedList;
         }
 
 
-        #endregion
+
+
+        /// <summary>
+        /// Token + Bid
+        /// </summary>    
+        public void setAllTokenBid(int TokenBidID, int BidID, string BidURI) //Background
+        {
+            var togehter = new Google.Protobuf.Collections.MapField<int, string>();
+            togehter[BidID] = BidURI;
+            State.AllTokenBids[TokenBidID] = new TokenBid { TokenID = BidID, BidURI = BidURI };
+        }
+        public override Int32Value InsertAllTokenBid(InsertTokenBidInput input) //inserting
+        {
+            string bidURI = input.BidURI;
+            string tokenID = input.TokenID;
+
+            CreateBid(bidURI);
+            setAllTokenBid(State.TokenBidIds.Value, int.Parse(tokenID), bidURI);
+            State.TokenBidIds.Value = State.TokenBidIds.Value + 1;
+            return new Int32Value { Value = State.TokenIds.Value };
+        }
+        public override SearchedListBids SearchAllBidByTokenID(StringValue tokenID) //Searching
+        {
+            var searchedListbids = new SearchedListBids();
+            for (int i = 0; i < State.TokenBidIds.Value; i++)
+            {
+                var TokenBid = State.AllTokenBids[i];
+                if (TokenBid.TokenID == int.Parse(tokenID.Value))
+                {
+                    searchedListbids.Bids.Add(TokenBid.BidURI);
+                }
+            }
+            return searchedListbids;
+        }
+
 
 
     }
--- generated/contract/DemeterGift/DemeterGiftContractState.cs
+++ generated/contract/DemeterGift/DemeterGiftContractState.cs
@@ -9,10 +9,15 @@ namespace DemeterGift
     {
         public Int32State EventIds { get; set; }
         public Int32State TokenIds { get; set; }
+        public Int32State BidIds { get; set; }
         public Int32State EventTokenIds { get; set; }
+        public Int32State TokenBidIds { get; set; }
         public MappedState<int, string> EventUris { get; set; }
         public MappedState<int, string> TokenUris { get; set; }
+        public MappedState<int, string> BidUris { get; set; }
         public MappedState<int, EventToken> AllEventTokens { get; set; }
+        public MappedState<int, TokenBid> AllTokenBids { get; set; }
         public MappedState<int, EventToken> SearchedEventTokens { get; set; }
+        public MappedState<int, TokenBid> SearchedTokenBids { get; set; }
     }
 }
